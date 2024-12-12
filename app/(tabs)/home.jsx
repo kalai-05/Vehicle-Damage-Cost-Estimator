a7260c974f79005f475a9b8469e9a49272636aaf
Harishka-11 <136185180+kalai-05@users.noreply.github.com>
2024-12-12 17:27:33 +0530

    Slider Done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
+import Slider from "../../components/Home/Slider";
 
 export default function home() {
   return (
     <View>
       <Header />
+      <Slider />
     </View>
   );
 }
